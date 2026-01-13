09ba1c5b89f107c1f5fbb78aac6e8764094606ec
Tilemat <manziro785@gmail.com>
2026-01-13 18:28:43 +0600

    added new deps and configurated tastack

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { RouterProvider } from "@tanstack/react-router";
 import { ThemeProvider } from "./app/providers/ThemeProvider";
 import { router } from "./app/routes";
 import './app/styles/global.css';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./app/lib/QueryClient";
+
 
 function App() {
 	return (
 		<ThemeProvider>
+			    <QueryClientProvider client={queryClient}>
+
 			<RouterProvider router={router} context={undefined} />
+			</QueryClientProvider>
 		</ThemeProvider>
 	);
 }
